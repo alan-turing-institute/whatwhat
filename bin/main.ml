b694e5f6a3650a695102857737b885a702bb24f1
Markus Hauru <mhauru@turing.ac.uk>
2022-07-28 18:16:05 +0100

    Rename github.ml -> githubRaw.ml

--- bin/main.ml
+++ bin/main.ml
@@ -4,8 +4,8 @@ open Whatwhat
 
 let () =
   let clients = ForecastRaw.getClients () in
-  let issues = Github.get_project_issues "NowWhat Test Project" in
+  let issues = GithubRaw.get_project_issues "NowWhat Test Project" in
   print_endline "Obtained clients:";
   List.iter (fun c -> print_endline @@ ForecastRaw.show_client c) clients;
   print_endline "Obtained issues:";
-  List.iter (fun c -> print_endline @@ Github.show_issue c) issues
+  List.iter (fun c -> print_endline @@ GithubRaw.show_issue c) issues
