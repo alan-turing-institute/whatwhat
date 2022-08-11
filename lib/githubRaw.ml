3984dbcdd18e16375cce9fe3a4541f6d5fef1744
Markus Hauru <mhauru@turing.ac.uk>
2022-08-11 19:37:19 +0100

    Style improvements to githubRaw.ml

--- lib/githubRaw.ml
+++ lib/githubRaw.ml
@@ -24,29 +24,29 @@ let member = Basic.Util.member
 let issue_of_json json =
   json |> member "node" |> member "content" |> fun x ->
   {
-    number = member "number" x |> Basic.Util.to_int;
-    title = member "title" x |> Basic.Util.to_string;
-    body = member "body" x |> Basic.Util.to_string;
-    state = member "state" x |> Basic.Util.to_string;
+    number = x |> member "number" |> Basic.Util.to_int;
+    title = x |> member "title" |> Basic.Util.to_string;
+    body = x |> member "body" |> Basic.Util.to_string;
+    state = x |> member "state" |> Basic.Util.to_string;
   }
 
 let column_of_json json =
   json |> member "node" |> fun x ->
   {
-    name = member "name" x |> Basic.Util.to_string;
+    name = x |> member "name" |> Basic.Util.to_string;
     cards =
-      member "cards" x |> member "edges"
+      x |> member "cards" |> member "edges"
       |> Basic.Util.convert_each (fun y ->
-             (issue_of_json y, member "cursor" y |> Basic.Util.to_string));
+             (issue_of_json y, y |> member "cursor" |> Basic.Util.to_string));
   }
 
 let project_of_json json =
   json |> member "node" |> fun x ->
   {
-    number = member "number" x |> Basic.Util.to_int;
-    name = member "name" x |> Basic.Util.to_string;
+    number = x |> member "number" |> Basic.Util.to_int;
+    name = x |> member "name" |> Basic.Util.to_string;
     columns =
-      member "columns" x |> member "edges"
+      x |> member "columns" |> member "edges"
       |> Basic.Util.convert_each column_of_json;
   }
 
@@ -67,61 +67,56 @@ let github_graph_ql_endpoint = "https://api.github.com/graphql"
    https://docs.github.com/en/graphql/guides/forming-calls-with-graphql#communicating-with-graphql
 *)
 let run_github_query (git_hub_token : string) body =
-  (* TODO The next line must be prettiable with the right use of @@ or |> or
-     similar *)
+  let auth_cred = Auth.credential_of_string ("Bearer " ^ git_hub_token) in
   let header =
-    Header.prepend_user_agent
-      (Header.add_authorization (Header.init ())
-         (Auth.credential_of_string ("Bearer " ^ git_hub_token)))
-      "NowWhat"
+    Header.init () |> fun header ->
+    Header.add_authorization header auth_cred |> fun header ->
+    Header.prepend_user_agent header "NowWhat"
   in
   let body_obj = Cohttp_lwt.Body.of_string body in
   let uri = Uri.of_string github_graph_ql_endpoint in
-  let response = Client.post ~headers:header ~body:body_obj uri in
-  response
+  Client.post ~headers:header ~body:body_obj uri
 
 (* Format JSON query to enable correct parsing on Github's side *)
 let format_query (q : string) = Str.global_replace (Str.regexp "\n") "" q
 
 (* ---------------------------------------------------------------------- *)
 
-let read_query_template () =
-  open_in "./queries/issues-by-project-graphql.json"
-  |> BatIO.lines_of |> BatEnum.fold ( ^ ) ""
+let query_template_path = "./queries/issues-by-project-graphql.json"
+
+let read_file_as_string filepath =
+  open_in filepath |> BatIO.lines_of |> BatEnum.fold ( ^ ) ""
 (* TODO Should we close the stream? *)
 
+let build_query project_name cursor =
+  (* Fill in placeholders into the query - project board name and cursor for
+     paging. *)
+  let query_template = read_file_as_string query_template_path in
+  let cursor_query =
+    let cursor_exp = Str.regexp "CURSOR" in
+    let replace_with =
+      match cursor with
+      | None -> "null" (* Get first batch *)
+      | Some crs -> "\\\"" ^ crs ^ "\\\"" (* Get subsequent batches *)
+    in
+    Str.global_replace cursor_exp replace_with query_template
+  in
+
+  let to_replace = Str.regexp "PROJECTNAME" in
+  let replace_with = "\\\"" ^ project_name ^ "\\\"" in
+  Str.global_replace to_replace replace_with cursor_query |> format_query
+
 let get_project_issues (project_name : string) =
-  (* the parent function is only wrapping up the recursive call that deals
-     with paging of the responses *)
+  (* This parent function is only wrapping up the recursive call that deals
+     with paging of the responses. *)
   let github_token = Config.settings.githubToken in
 
   let rec get_project_issues_page (project_name : string) cursor
       (acc : (issue * string) list) =
-    let query_template = read_query_template () in
-
-    (* fill in placeholders into the query - project board name and cursor for paging *)
-    let query =
-      let cursor_query =
-        let cursor_exp = Str.regexp "CURSOR" in
-        match cursor with
-        | None ->
-            (* Get first batch *)
-            Str.global_replace cursor_exp "null" query_template
-        | Some crs ->
-            (* Get subsequent batches *)
-            (* TODO Figure out how to do string interpolation nicely. *)
-            Str.global_replace cursor_exp ("\\\"" ^ crs ^ "\\\"") query_template
-      in
-
-      (* TODO Figure out how to do string interpolation nicely. *)
-      Str.global_replace (Str.regexp "PROJECTNAME")
-        ("\\\"" ^ project_name ^ "\\\"")
-        cursor_query
-      |> format_query
-    in
-
+    let query = build_query project_name cursor in
     (* TODO We should probably check the response to see that it was successful. *)
     let _, body = run_github_query github_token query |> Lwt_main.run in
+    (* Parse the response into a list of issues. *)
     let issues =
       body |> Cohttp_lwt.Body.to_string |> Lwt_main.run |> Basic.from_string
       |> project_root_of_json
