5d3c1769e2e5bfd48369f1e00bb953bbd2596676
Markus Hauru <mhauru@turing.ac.uk>
2022-09-02 16:27:49 +0100

    Docstring for dateprinter.ml

--- lib/dateprinter.ml
+++ lib/dateprinter.ml
@@ -1,3 +1,7 @@
+(** A module with two functions: [dateprinter], which pretty-prints a [CalendarLib.Date]
+    object, and [dateprint_opt] which does the same for a [CalendarLib.Date option],
+    returning ["None"] for [None].
+    *)
 open CalendarLib
 
 let dateprinter fmt (dt : Date.t) = Format.pp_print_string fmt (Printer.Date.to_string dt)
