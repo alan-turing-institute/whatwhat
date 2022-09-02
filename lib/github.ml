229c7bdf85a9577b0788c3f59a986a81e434a2f0
callummole <cmole@turing.ac.uk>
2022-09-02 17:29:09 +0100

    WIP restructure parsing

--- lib/github.ml
+++ lib/github.ml
@@ -38,10 +38,12 @@ type parseerror =
 
 let log_parseerror (what: parseerror) (number: int) msg =
   match what with
-  | LengthError -> print_endline @@ "Error: Metadata Parsing (num: " ^ (string_of_int number) ^ "): Expected 8 metadata keys, got " ^ msg 
+  | LengthError -> print_endline @@ "Error: Metadata Parsing (num: " ^ (string_of_int number) ^ "):"
   | LineError -> print_endline @@ "Error: Metadata Parsing (num: " ^ (string_of_int number) ^ "): Unable to break line into (key, value) - " ^ msg
   | FieldError -> print_endline @@ "Error: Metadata Parsing (num: " ^ (string_of_int number) ^ "): Unable to parse key " ^ msg
   | FieldWarning -> print_endline @@ "Warning: Metadata Parsing (num: " ^ (string_of_int number) ^ "): key " ^ msg
+
+
 
 (* ---------------------------------------------------------------------- *)
 (* TYPES *)
@@ -55,8 +57,8 @@ let dateprint_opt (dt: Date.t option) =
 
 type metadata = 
   { turing_project_code : string option
-  ; earliest_start_date : Date.t option [@printer fun fmt x -> Format.pp_print_string fmt (dateprint_opt x)]
-  ; latest_start_date : Date.t option [@printer fun fmt x -> Format.pp_print_string fmt (dateprint_opt x)]
+  ; earliest_start_date : Date.t [@printer dateprinter]
+  ; latest_start_date : Date.t [@printer  dateprinter]
   ; latest_end_date : Date.t option [@printer fun fmt x -> Format.pp_print_string fmt (dateprint_opt x)]
   ; fte_months : float option
   ; nominal_fte_percent : float option
@@ -86,79 +88,111 @@ type project =
 [@@deriving show]
 
 
+type field = {
+  name : string;
+  optional : bool; (* if False, the issue will not become a NowWhat project is this field does not exist *)  
+  } 
+
+(* This is useful for knowing what is a key field.  *)
+let metadata_fields = [
+    {name = "turing-project-code"; optional = true};
+    {name = "earliest-start-date"; optional = true};
+    {name = "latest-start-date"; optional = true};
+    {name = "latest-end-date"; optional = true};
+    {name = "FTE-months"; optional = true};
+    {name = "nominal-FTE-percent"; optional = true};
+    {name = "max-FTE-percent"; optional = true};
+    {name = "min-FTE-percent"; optional = true}
+  ]
+
 (* ---------------------------------------------------------------------- *)
 (* METADATA PARSING & VALIDATION *)
 
 
-(* --- Convenience functions ---- *)
-
-let maybe_null ~(f: string -> 'a) (x: string option) =
-  match x with
-  | None -> None
-  | Some y -> match y with 
-              | "" -> None
-              | "null" -> None
-              | _ -> Some (f y)
-
-let maybe_null_string s = maybe_null ~f:(fun x -> x) s
-let maybe_null_float s = maybe_null ~f:float_of_string s 
-
-let catch_date_exn year month day n =
-  try Some (Date.make year month day) with
-    Date.Out_of_bounds -> log_parseerror FieldError n "date out of bounds"; None
+(* --- field parsing functions ---- *)
 
 
-let make_date (n: int) (str: string option) = 
-  match str with
-  | None -> log_parseerror  FieldError n "key does not exist"; None
-  | Some x -> let datelist = Str.split (Str.regexp {|-|}) x in
+let date_from_string (n: int) (str: string) = 
+  let catch_date_exn year month day n =
+    try Some (Date.make year month day) with
+      Date.Out_of_bounds -> log_parseerror FieldError n "date out of bounds"; None 
+  in
+  let datelist = Str.split (Str.regexp {|-|}) str in
     match datelist with
     | year :: month :: day :: [] -> catch_date_exn (int_of_string year) (int_of_string month) (int_of_string day) n   
-    | _ -> log_parseerror  FieldError n x; None
+    | _ -> log_parseerror  FieldError n str; None
+ 
 
-  
-(* the value should be of the form " <val>". Any other spaces then content indicates a violation*)
-let check_value (n: int) (k: string) (v: string) =
-  let x = Str.split (Str.regexp " ") v in
+let date_from_string_opt (n: int) (str: string option) =
+    match str with
+    | None -> None
+    | Some x -> date_from_string n x
+   
+let float_opt_of_string_opt (x: string option) =
   match x with
-  | [] -> log_parseerror  FieldWarning n (k ^ ", empty value"); ""
-  | "null"::[] -> log_parseerror  FieldWarning n (k ^ ", null value"); ""
+  | Some "null" -> None  (* not sure why there are still nulls at this stage...should be converted to empty strings in check_value? *)
+  | Some "" -> None
+  | None -> None
+  | Some y -> Some (float_of_string y)
+
+(* the value should be of the form " <val>". Any other spaces then content indicates a violation*)
+(* at this stage an empty string to indicate no value*)
+let check_value (n: int) (key: string) (value: string) =
+  let x = Str.split (Str.regexp " ") value in
+  match x with
+  | [] -> log_parseerror  FieldWarning n (key ^ ", empty value"); ""
+  | "null"::[] -> log_parseerror  FieldWarning n (key ^ ", null value"); ""
   | y::[] -> y
-  | y::z -> log_parseerror FieldWarning n (k ^ ", additional info - " ^ (String.concat "" z)); y
+  | y::z -> let info = Str.global_replace (Str.regexp " *") "" (String.concat "" z) in 
+              match info with 
+              | "" -> y
+              | _  -> log_parseerror FieldWarning n (key ^ ", additional info - " ^ (String.concat "" z)); y
 
 
 let list_to_pair (n: int) (x: string list) =
   match x with
-  | [k;v] -> (k, (check_value n k v))
-  | y -> log_parseerror LineError n (String.concat ";"  y); ("", "")
+  | [key;value] -> Some (key, (check_value n key value))
+  | [key] -> log_parseerror  FieldWarning n (key ^ ", empty value"); Some (key, "")
+  | y -> log_parseerror LineError n (String.concat ";"  y); None
 
 (* ---  *)
-  
+
+let key_exists yaml_fields mfield =
+    if mfield.optional then true else List.mem_assoc mfield.name yaml_fields
+
 let parse_fields (n: int) (lines: string list) =
   
-  let fields = List.map (fun x -> Str.split (Str.regexp {|:|}) x |> list_to_pair n) lines in
-  Some { turing_project_code = fields |> List.assoc_opt "turing-project-code" |> maybe_null_string
-    ; earliest_start_date = fields |> List.assoc_opt "earliest-start-date" |> make_date n
-    ; latest_start_date = fields |> List.assoc_opt "latest-start-date" |> make_date n
-    ; latest_end_date = fields |> List.assoc_opt "latest-end-date" |> make_date n
-    ; fte_months = fields |> List.assoc_opt "FTE-months" |> maybe_null_float
-    ; nominal_fte_percent = fields |> List.assoc_opt "nominal-FTE-percent" |> maybe_null_float
-    ; max_fte_percent = fields |> List.assoc_opt "max-FTE-percent" |> maybe_null_float
-    ; min_fte_percent = fields |> List.assoc_opt "min-FTE-percent" |> maybe_null_float
-  } 
+  (* we now know if the value exists *)
+  let fields = List.filter_map (fun x -> Str.split (Str.regexp {|:|}) x |> list_to_pair n) lines in
 
-  
+  (* we now know if the essential keys exist *)
+  let contains_key_fields = List.for_all (key_exists fields) metadata_fields in
+  if contains_key_fields then
+    (* parse data *)
+    Some { turing_project_code = fields |> List.assoc_opt "turing-project-code"
+      ; earliest_start_date = fields |> List.assoc "earliest-start-date" |> date_from_string n |> Option.get
+      ; latest_start_date = fields |> List.assoc "latest-start-date" |> date_from_string n |> Option.get
+      ; latest_end_date = fields |> List.assoc_opt "latest-end-date" |> date_from_string_opt n
+      ; fte_months = fields |> List.assoc_opt "FTE-months" |> float_opt_of_string_opt
+      ; nominal_fte_percent = fields |> List.assoc_opt "nominal-FTE-percent" |> float_opt_of_string_opt
+      ; max_fte_percent = fields |> List.assoc_opt "max-FTE-percent" |> float_opt_of_string_opt
+      ; min_fte_percent = fields |> List.assoc_opt "min-FTE-percent" |> float_opt_of_string_opt
+    } 
+  else
+    (log_parseerror FieldError n "Essential fields missing";
+    None)
+
   
 let metadata_of_yaml (n: int) (y: string) = 
   let lines = Str.split (Str.regexp "\r\n") y in
-  let len = List.length lines in 
-  match len with 
-  | 8 -> parse_fields n lines 
-  | _ -> log_parseerror LengthError n (string_of_int len); None
+  let lenlines = List.length lines in 
+  let lenfields = List.length metadata_fields in
+  if lenlines != lenfields then log_parseerror LengthError n ("length of yaml is" ^ (string_of_int lenlines) ^ " but length of expected metadata fields is " ^ (string_of_int lenfields));
+  parse_fields n lines
 
 
 let parse_metadata (n: int) (body: string) =
-  let x = Str.split (Str.regexp {|+++|}) body in
+  let x = Str.split (Str.regexp {|\+\+\+|}) body in
   match x with
   | top :: rest :: [] -> let mdata = top |> metadata_of_yaml n in (mdata, Some rest) 
   | _ -> (None, None) 
--- lib/github.mli
+++ lib/github.mli
@@ -1,6 +1,5 @@
 
 
-open CalendarLib
 module Raw = GithubRaw
 
 type parseerror =
@@ -11,8 +10,8 @@ type parseerror =
 
 type metadata = {
     turing_project_code : string option;
-    earliest_start_date : CalendarLib.Date.t option;
-    latest_start_date : CalendarLib.Date.t option;
+    earliest_start_date : CalendarLib.Date.t ;
+    latest_start_date : CalendarLib.Date.t ;
     latest_end_date : CalendarLib.Date.t option;
     fte_months : float option;
     nominal_fte_percent : float option;
@@ -39,15 +38,6 @@ type project = {
 
 
 val log_parseerror : parseerror -> int -> string -> unit
-val dateprinter: Format.formatter -> Date.t -> unit
-val dateprint_opt: Date.t option -> string
-val maybe_null: f:(string -> 'a) -> string option -> 'a option
-val maybe_null_string: string option -> string option
-val maybe_null_float: string option -> float option
-val catch_date_exn: int -> int -> int -> int -> Date.t option
-val make_date: int -> string option -> Date.t option
-val check_value: int -> string -> string -> string
-val list_to_pair: int -> string list -> string * string
 val parse_fields: int -> string list -> metadata option
 val metadata_of_yaml: int -> string -> metadata option
 val parse_metadata: int -> string -> metadata option * string option
