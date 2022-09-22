496cd0fe67f057d5e425009fbace3855a2c95433
= <jgeddes@turing.ac.uk>
2022-09-22 13:52:35 +0100

    Add documentation for Github module

--- lib/github.mli
+++ lib/github.mli
@@ -1,31 +1,45 @@
-(** High-level interface to the Github API
+(** High-level interface to the Project Tracker in Github.
 
-Performs validation for Github issues.
+    GitHub is used to record the overall status of each project. Each project is
+    represented as an issue and indeed the GitHub issue number (sometimes
+    referred to as a "Hut23 code") is used as a unique identifier for projects,
+    including those on Forecast.
 
-Errors are logged to the console when metadata is malformed, and the issue is dropped from
-the list of all issues.
+    A GitHub issue consists of a title, a body (the main content of the issue),
+    some metadata (such as the people assigned to the issue and the "column" to
+    which the issue belongs) and then a list of additional comments.
 
-Errors currently occur when:
-  - Metadata does not have 8 keys.
-  - Cannot break a metadata line into a (key,value) pair, which means it is incorrect
-  yaml.
-  - A crucial key cannot be parsed (missing, or there is an error when dealing with the
-  value). Currently all keys are marked as non-crucial, but this may change in the future.
+    We use the body of the issue to hold project-level metadata encoded as a
+    YAML block (described below). The additional comments are not returned by
+    this module.
+    
+    {2 Validation and reporting}
 
-Warnings are given for other inconsisties (e.g. no one is assigned). Warnings are given
-due to:
-  - There being additional information in the value entry.
-  - A non-crucial entry is missing or null. *)
-module Raw = GithubRaw
+    Errors are logged to the console when metadata is malformed, and the issue
+    is dropped from the list of all issues.
 
-(** A type for different types of errors that may be raised when parsing issue metadata. *)
+    Errors currently occur when:
+      - Metadata does not have 8 keys.
+      - Cannot break a metadata line into a (key,value) pair, which means it is
+        incorrect yaml.
+      - A crucial key cannot be parsed (missing, or there is an error when dealing
+        with the value). Currently all keys are marked as non-crucial, but this may
+        change in the future.
+
+    Warnings are given for other inconsisties (e.g. no one is
+    assigned). Warnings are given due to:
+      - There being additional information in the value entry.
+      - A non-crucial entry is missing or null.
+
+ *)
+
 type parseerror =
   | FieldError
   | FieldWarning
   | LengthWarning
-  | LineError
+  | LineError (** *)
+(** Errors and warnings for logging problems with the issue metadata. *)
 
-(** A type to hold the parsed YAML metadata from an issue header. *)
 type metadata =
   { turing_project_code : string option
   ; earliest_start_date : CalendarLib.Date.t option
@@ -37,17 +51,22 @@ type metadata =
   ; min_fte_percent : float option
   }
 
+val show_metadata : metadata -> string
+(** A type to hold the parsed YAML metadata from an issue header. *)
+
 (* We reexport the Raw.person type so that no other module ever has a need to import
    anything from GithubRaw. *)
 
-(** A type for Github users. *)
-type person = Raw.person =
+type person = GithubRaw.person =
   { login : string
   ; name : string option
   ; email : string option
   }
 
-(** Projects are 1-to-1 related with Github issues. *)
+val show_person : person -> string
+(** A type for Github users. *)
+
+
 type project =
   { number : int
   ; title : string
@@ -59,13 +78,13 @@ type project =
   ; metadata : metadata
   }
 
+val show_project : project -> string
+(** Projects are 1-to-1 related with Github issues. *)
+
+
+val get_project_issues : string -> project list
 (** Given a project board name, return a list of projects, one for each issue on the
     board. *)
-val get_project_issues : string -> project list
 
-(** Return all the users in the Alan Turing Institute Github organisation. *)
 val get_users : unit -> person list
-
-val show_project : project -> string
-val show_metadata : metadata -> string
-val show_person : person -> string
+(** Return all the users in the Alan Turing Institute Github organisation. *)
