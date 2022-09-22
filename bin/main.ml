dde34e58ed9b03d674d6df20ec7349eda24d6486
= <jgeddes@turing.ac.uk>
2022-09-22 09:26:31 +0100

    Minor changes to main.ml placeholder

--- bin/main.ml
+++ bin/main.ml
@@ -3,9 +3,9 @@
 open Whatwhat
 
 let () =
-  let clients = ForecastRaw.getClients () in
+  (* let clients = ForecastRaw.getClients () in *)
   let issues = GithubRaw.get_project_issues "NowWhat Test Project" in
-  print_endline "Obtained clients:";
-  List.iter (fun c -> print_endline @@ ForecastRaw.show_client c) clients;
+  (* print_endline "Obtained clients:"; *)
+  (* List.iter (fun c -> print_endline @@ ForecastRaw.show_client c) clients; *)
   print_endline "Obtained issues:";
   List.iter (fun c -> print_endline @@ GithubRaw.show_issue c) issues
