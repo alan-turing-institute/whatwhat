0bc45ea7b189f011645884570a31bd30c815346b
Markus Hauru <mhauru@turing.ac.uk>
2022-09-22 10:50:30 +0100

    More docstrings

--- lib/githubRaw.mli
+++ lib/githubRaw.mli
@@ -1,15 +1,16 @@
 (** GithubRaw queries the Github GraphQL API for data about issues, users, and
-    project boards, and parses that into OCaml records. It throws exceptions if
-    the data is too broken for even this, but doesn't otherwise check for
-    consistency or quality of what it receives.
-   *)
+    project boards, and parses that into OCaml records. It throws exceptions if the data
+    is too broken for even this parsing, but doesn't otherwise check for consistency or
+    quality of what it receives. *)
 
+(** A Github user. *)
 type person =
   { login : string
   ; name : string option
   ; email : string option
   }
 
+(** A Github issue. *)
 type issue =
   { number : int
   ; title : string
@@ -20,19 +21,21 @@ type issue =
   ; column : string option
   }
 
+(** A column in a Github project (a Kanban board). *)
 type column =
   { name : string
   ; cards : (issue * string) list
   }
 
 (** Project is what Github calls Kanban boards. It is _not_ what the rest of
-    Whatwhat calls project, which is more like an issues.*)
+    Whatwhat calls project, which is more like an issue. *)
 type project =
   { number : int
   ; name : string
   ; columns : column list
   }
 
+(** A list of Github projects in a repository. *)
 type project_root = { projects : project list }
 
 val show_person : person -> string
@@ -41,8 +44,7 @@ val show_column : column -> string
 val show_project : project -> string
 val show_project_root : project_root -> string
 
-(** Return the list of issues in a project board, given the name of the board.
-    *)
+(** Return the list of issues in a project board, given the name of the board. *)
 val get_project_issues : string -> issue list
 
 (** Return all the users in the Alan Turing Institute Github organisation. *)
--- lib/schedule.mli
+++ lib/schedule.mli
@@ -1,9 +1,21 @@
+(** Schedule collates data from both Github and Forecast, and joins them into single
+    source-agnostic data structure. It deals with people, projects, and allocations.
+    *)
+
+(** A person, combining both a Forecast user and a matching Github user.
+
+    Forecast is considered the authoritative source for people, and Github logins are
+    attached to the Forecast information. *)
 type person =
   { email : string
   ; name : string
   ; github_login : string
   }
 
+(** A project, combining both a Github issue and a matching Forecast project.
+
+    Github is considered the authoritative source for projects, information from
+    Forecast is attached to the issues found on Github. *)
 type project =
   { forecast_id : int
   ; github_id : int
@@ -22,7 +34,7 @@ type project =
   }
 
 (** An allocation of a person to a project for a time period. [rate] is the number of
-    hours per day this allocation covers.*)
+    hours per day this allocation covers. *)
 type allocation =
   { person_id : int
   ; project_id : int
@@ -31,6 +43,13 @@ type allocation =
   ; rate : float
   }
 
+(** Return a list of people and projects for which we succesfully merged Forecast and
+    Github data. In the process of doing the merge, log various warnings and errors when
+    data on Forecast and/or Github is missing or malformed.
+
+    TODO This function remains a work-in-progress. For one, we need to have it return a
+    list of allocations as well. *)
 val make_schedule : unit -> person list * project list
+
 val show_person : person -> string
 val show_project : project -> string
